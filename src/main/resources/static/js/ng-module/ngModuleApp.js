c60bd7d0dabf6bcbcdbaf1c37cc5e46be0678cbf
nico <nico@localhost>
2017-09-20 12:07:22 +0200

    Creacion de los archivos dinamicos para hacer formularios dinamicos.

--- src/main/resources/static/js/ng-module/ngModuleApp.js
+++ src/main/resources/static/js/ng-module/ngModuleApp.js
@@ -1,36 +1,25 @@
-var app = angular.module('app', ['ngRoute', 'ngSanitize']);
+var app = angular.module('app', [ 'ngRoute', 'ngSanitize' ]);
 
-
-app.config(['$routeProvider', '$locationProvider',
-  function ($routeProvider, $locationProvider)
-  {
-    $routeProvider.otherwise(
-    {
-      redirectTo: '/'
-    })
-    .when('/',
-    {
-      templateUrl: GBL_COFG.urlTemplate('ini.html')
-    })
-    .when('/about',
-    {
-      templateUrl: GBL_COFG.urlTemplate('about.html'),
-      controller: 'ngAppControllerAbout'
-    })
-    .when('/test-bbdd',
-    {
-      templateUrl: GBL_COFG.urlTemplate('test-bbdd.html'),
-      controller: 'ngAppControllerTestBbdd'
-    })
-    .when('/kirby',
-    {
-      templateUrl: GBL_COFG.urlTemplate('kirby.html')
-    })
-    .when('/search',
-    {
-      templateUrl: GBL_COFG.urlTemplate('search.html'),
-      controller: 'ngAppControllerSearch'
-    });
-  }]);
-
-
+app.config([ '$routeProvider', '$locationProvider',
+	function($routeProvider, $locationProvider) {
+		$routeProvider.otherwise({
+			redirectTo : '/'
+		}).when('/', {
+			templateUrl : GBL_COFG.urlTemplate('ini.html')
+		}).when('/about', {
+			templateUrl : GBL_COFG.urlTemplate('about.html'),
+			controller : 'ngAppControllerAbout'
+		}).when('/test-bbdd', {
+			templateUrl : GBL_COFG.urlTemplate('test-bbdd.html'),
+			controller : 'ngAppControllerTestBbdd'
+		}).when('/kirby', {
+			templateUrl : GBL_COFG.urlTemplate('kirby.html')
+		}).when('/editBBDD', {
+			templateUrl : GBL_COFG.urlTemplate('editbbdd.html'),
+			controller : 'ngAppControllerEditBBDD'
+			
+		}).when('/search', {
+			templateUrl : GBL_COFG.urlTemplate('search.html'),
+			controller : 'ngAppControllerSearch'
+		});
+	} ]);
